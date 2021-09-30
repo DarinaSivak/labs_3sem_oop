3baaae4c0406bcd9b72d3fec5ff0a7324708b2ae
Blood Shake <89980619+DarinaSivak@users.noreply.github.com>
2021-09-30 11:17:51 +0300

    3 laba

---
+++ lab3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            airline flight1 = new airline ("Санкт-Петербург", 29, "Bombardier", "10:30", "суббота", "Санкт-Петербург", "пятница");
+            airline flight2 = new airline ("Лондон", 875, "Embraer", "23:46", "понедельник", "Санкт-Петербург", "понедельник");
+            airline flight3 = new airline ("Санкт-Петербург", 12, "ARJ-21", "19:34", "четверг", "Лондон", "среда");
+
+            airline[] flights = new airline[3];
+            flights[0] = flight1;
+            flights[1] = flight2;
+            flights[2] = flight3;
+
+            airline.NumOfAirline();
+
+            airline closed = new airline(); //вызов закрытого конструктора
+
+            Partial newPartial = new Partial();
+            newPartial.partialFunc();
+
+            Console.WriteLine("GetType : " + flight1.GetType());
+            Console.WriteLine("ToString : " + flight1.ToString());
+            Console.WriteLine("GetHashCode : " + flight1.GetHashCode());
+            Console.WriteLine("Equals: " + flight1.Equals(flight2));
+
+            Console.WriteLine("введите пункт назначения : ");
+            string consoleDest = Console.ReadLine();
+            Console.WriteLine("список рейсов для заданного пункта назначения : ");
+            for (int i = 0; i < flights.Length; i++)
+            {
+                if (flights[i].destination == consoleDest)
+                {
+                    Console.WriteLine("номер рейса : " + flights[i].flightNumber);
+                }
+            }
+
+            Console.WriteLine("введите день недели : ");
+            string consoleDay = Console.ReadLine();
+            Console.WriteLine("список рейсов для заданного дня недели : ");
+            for (int i = 0; i < flights.Length; i++)
+            {
+                if (flights[i].weekday == consoleDay)
+                {
+                    Console.WriteLine("номер рейса : " + flights[i].flightNumber);
+                }
+            }
+
+            var anonymousType = new {destination = "Москва", departureTime = "06:21", flightNumber = 95};
+            Console.WriteLine("анонимный тип : " + anonymousType.GetType());
+        }
+    }
+}
---
+++ lab3/airline.cs
@@ -0,0 +1,209 @@
+using System;
+
+namespace lab3 
+{
+    public class airline
+    {
+        public string destination;
+        public string Destination 
+        {
+            set 
+            {
+                if (destination == null)
+                {
+                    Console.WriteLine("пункт назначения не задан !");
+                }
+                else
+                {
+                    destination = value;
+                }
+            }
+            get 
+            {
+                return destination;
+            }
+        }
+
+        public int flightNumber;
+        public int FlightNumber { get; set; }
+
+        public string typeOfAirplane;
+        public string TypeOfAirplane 
+        {
+            set 
+            {
+                if (typeOfAirplane == null)
+                {
+                    Console.WriteLine("тип самолета не задан !");
+                }
+                else
+                {
+                    typeOfAirplane = value;
+                }
+            }
+            get 
+            {
+                return typeOfAirplane;
+            }
+        }
+
+        public string departureTime;
+        public string DepartureTime
+        {
+            set 
+            {
+                if (departureTime == null)
+                {
+                    Console.WriteLine("время вылета не задано !");
+                }
+                else
+                {
+                    departureTime = value;
+                }
+            }
+            get 
+            {
+                return departureTime;
+            }
+        }
+
+        public string weekday;
+        public string Weekday
+        {
+            set 
+            {
+                if (weekday == null)
+                {
+                    Console.WriteLine("день недели не задан !");
+                }
+                else
+                {
+                    weekday = value;
+                }
+            }
+            get 
+            {
+                return weekday;
+            }
+        }
+
+        static short numOfAirline = 0;
+
+        static airline() //статический конструктор
+        {
+            Console.WriteLine("вызван статический конструктор");
+        }
+
+        public airline (string destination, int flightNumber, string typeOfAirplane, string departureTime, string weekday, string destinationRef, string weekdayRefOut) //конструктор с параметрами
+        {
+            this.destination = destination;
+            this.flightNumber = flightNumber;
+            this.typeOfAirplane = typeOfAirplane;
+            this.departureTime = departureTime;
+            this.weekday = weekday;
+            this.destinationRef = destinationRef;
+            this.weekdayRefOut = weekdayRefOut; 
+            numOfAirline++;
+            outPut();
+            destRef(ref destinationRef);
+            dayRef(ref weekdayRefOut);
+            dayOut(out weekdayRefOut);
+        }
+
+        private string destinationRef;
+        public string DestinationRef
+        {
+            get {return destinationRef; }
+            set {}
+        }
+
+        private string weekdayRefOut;
+        public string WeekdayRefOut { get; set; }
+
+        public void destRef(ref string destinationRef)
+        {
+            if (destination == destinationRef)
+            {
+                Console.WriteLine($"номер рейса : {flightNumber}");
+            }
+        }
+        
+        public void dayRef(ref string weekdayRefOut)
+        {
+            if (weekday == weekdayRefOut)
+            {
+                Console.WriteLine($"номер рейса : {flightNumber}");
+            }
+        }
+
+        public void dayOut(out string weekdayRefOut)
+        {
+            weekdayRefOut = this.weekdayRefOut;
+        }
+
+        public void outPut()
+        {
+            Console.WriteLine($"\nпункт назначения : {destination}");
+            Console.WriteLine($"номер рейса : {flightNumber}");
+            Console.WriteLine($"тип самолета : {typeOfAirplane}");
+            Console.WriteLine($"время вылета : {departureTime}");
+            Console.WriteLine($"день недели : {weekday}\n");
+        }
+
+        private airline() //конструктор без параметров
+        {
+            destination = "Светлогорск";
+            flightNumber = 293;
+            typeOfAirplane = "Boing";
+            departureTime = "23:12";
+            weekday = "пятница";
+            numOfAirline++;
+        }
+
+        public airline (string parametr = "default") {} //конструктор с параметром по умолчанию
+
+        public const double E = 2.71; //поле константа
+
+        public readonly int readOnly = 39; //поле только для чтения
+        public airline (int ReadOnly)
+        {
+            readOnly = ReadOnly; // поле для чтения может быть инициализировано или изменено в конструкторе после компиляции
+        }
+
+        public static int ID { get; private set; } = 54;
+
+        public static void NumOfAirline() //кол-во объектов
+        {
+            Console.WriteLine($"количество созданных объектов: {numOfAirline}");
+        }
+
+        public override string ToString()
+        {
+            return $"пункт назначения: {destination}, номер рейса: {flightNumber}, тип самолета: {typeOfAirplane}, время вылета: {departureTime}, день недели: {weekday}";
+        }
+
+        public override int GetHashCode()
+        {
+            return flightNumber.GetHashCode();
+        }
+
+        public bool Equals(airline obj)
+        {
+            airline plane;
+            if (obj == null || (plane = obj as airline) == null)
+            {
+                return false;
+            }
+            return this.destination == plane.destination;
+        }
+
+    }
+
+    public partial class Partial
+    {
+        public void partialFunc()
+        {
+                Console.WriteLine("частичный класс");
+        }
+    }
+}
